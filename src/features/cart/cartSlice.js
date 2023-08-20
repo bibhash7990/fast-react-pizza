1fecde5beaa42afabd2d9d6d417baeae5538aaed
bibhash <bibhash88lenka@gmail.com>
2023-08-20 08:11:12 +0530

    final

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -27,6 +27,8 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -53,3 +55,6 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 // reselect library for performance optimization
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
